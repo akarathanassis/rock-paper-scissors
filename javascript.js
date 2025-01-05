5547ca1f7bc93645e74d54dce9ec46b831abf1eb
Alex Kara <190240100+akarathanassis@users.noreply.github.com>
2025-01-05 12:06:20 +1100

    Define humanChoice function
    Need to add invalid input defense later

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-console.log("Hello World")
-
 function getComputerChoice() { 
     let value = Math.random(); 
 
@@ -14,6 +12,27 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() { 
+    let choice = prompt("Rock, paper or scissors?"); 
+
+    choice = choice.toLowerCase; 
+    while (choice) { 
+        if (choice == "rock") { 
+            return "rock"
+        }
+        else if (choice == "paper") { 
+            return "paper"
+        }
+        else if (choice == "scissors") { 
+            return "scissors"
+        }
+        else { 
+            alert("Invalid input! Enter the word rock, paper or scissors!");
+        }
+    }
+}
+
+let humanChoice = getHumanChoice(); 
 console.log(getComputerChoice())
 
 
