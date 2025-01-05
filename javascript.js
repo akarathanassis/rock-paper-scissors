6bf1a59c22a6380e8b9749b0f484c99678f0ecbe
Alex Kara <190240100+akarathanassis@users.noreply.github.com>
2025-01-05 11:56:01 +1100

    Create and add logic to getComputerChoice
    Randomly returns rock, paper or scissors

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
 console.log("Hello World")
 
+function getComputerChoice() { 
+    let value = Math.random(); 
+
+    if (value <= 0.33) { 
+        return "rock"
+    }
+    else if (value >= 0.33 && value <= 0.67) { 
+        return "paper"
+    }
+    else { 
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
+
 
